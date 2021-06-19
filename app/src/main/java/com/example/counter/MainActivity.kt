56e1adc135a04a3ad25c44db8b40c6ee23a771c6
kanhayaky <kaku18is@cmrit.ac.in>
2021-06-19 19:55:26 +0530

    First Commit

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -4,43 +4,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private val start = findViewById<Button>(R.id.startButton)
-    private val stop = findViewById<Button>(R.id.stopButton)
-    private val counter = findViewById<TextView>(R.id.counterValue)
-    private lateinit var handler: Handler
-    private lateinit var runnable:Runnable
+    private lateinit var mainHandler: Handler
+    var count : Int = 0
 
+
+    private val counter = object : Runnable {
+
+        override fun run() {
+            count+=1
+            findViewById<TextView>(R.id.counterValue).text = count.toString()
+            mainHandler.postDelayed(this, 1000)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addListeners()
+        mainHandler = Handler(Looper.getMainLooper())
+        addListener();
     }
 
-    private fun counter(button: Button){
-        button.isEnabled = false
-        when(button.id.toString()){
-            "Start" -> {
-                stop.isEnabled=true
-            }
-            "Stop" ->{
-                start.isEnabled=true
-            }
-        }
 
+    private fun stop(){
+        count=0
+        mainHandler.removeCallbacks(counter)
+        findViewById<TextView>(R.id.counterValue).text = count.toString()
     }
 
-    private fun addListeners() {
-        val clickableViews: List<View> = listOf(start, stop)
-        for (item in clickableViews) {
-            item.setOnClickListener { counter(it as Button) }
-        }
+    private fun addListener(){
 
-
+        val start = findViewById<Button>(R.id.startButton)
+        val stop = findViewById<Button>(R.id.stopButton)
+        start.setOnClickListener{
+            start.isEnabled = false
+            mainHandler.post(counter)}
+        stop.setOnClickListener{
+            start.isEnabled = true
+            stop()}
     }
 
 }
